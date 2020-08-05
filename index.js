ae1c2db5ee0b6801cf9896237e564d78287f6c2a
TheDudeFromCI <thedudefromci@gmail.com>
2020-08-05 01:08:33 -0700

    Created loot table object.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-module.exports.helloWorld = function () {
-  console.log('Hello world !')
+function removeNamespace (value) {
+  if (value.startsWith('minecraft:')) return value.substring(10)
+  else return value
 }
+
+class LootTable {
+  /**
+   * Creates a new loot table parser from the given Minecraft loot table.
+   *
+   * @param lootTable - The raw Minecraft loot table object.
+   */
+  constructor (lootTable) {
+    this._lootTable = lootTable
+    this.type = removeNamespace(lootTable.type)
+  }
+
+  /**
+   * Gets the type of loot table this is.
+   */
+  get type () {
+    return this.type
+  }
+}
+
+module.exports = { LootTable }
